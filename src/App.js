5e7aa5af4367361dde6300732b2971b1948f0e1c
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2023-05-31 23:29:01 +0530

    search box functionality in prototype stage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
-import Card from "./Card";
 import CardList from "./CardList";
 import { robots } from "./robots.js";
-function App() {
-  return <CardList robots={robots} />;
+import SearchBox from "./SearchBox";
+import React from "react";
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: "",
+    };
+  }
+  handleSearch(event) {
+    console.log(event.target.value);
+  }
+  render() {
+    return (
+      <div className="tc ">
+        <h1>RoboFriends</h1>
+        <SearchBox onSearchChange={this.handleSearch} />
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
 }
 
 export default App;
