af9f25d29e0218a42e3941ad0e49a5141b62fa45
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2023-05-31 12:03:34 +0530

    added robot.js and created random card elements

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,17 @@
 import React from "react";
-
+import "tachyons";
 class Card extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="tc bg-light-green dib br3 ma2 pa3 grow shadow-3">
+        <img
+          src={`https://robohash.org/${this.props.id}?200x200`}
+          alt="robot"
+        />
+        <h2>{this.props.name}</h2>
+        <p>{this.props.email}</p>
+      </div>
+    );
   }
 }
 
