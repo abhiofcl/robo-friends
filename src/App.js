10a7471e7bacf2eaa8c1721b22e85f8c02eaaa1c
Abhishek V <88491499+abhiofcl@users.noreply.github.com>
2023-06-01 20:41:03 +0530

    styled the ui

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
     });
     return (
       <div className="tc ">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox onSearchChange={this.handleSearch} />
         <CardList robots={filteredRobot} />
       </div>
